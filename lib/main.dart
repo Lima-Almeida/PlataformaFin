0dd6d973f3b57d7361c61faeeaaecad15911da46
carolkekas28 <a246914@dac.unicamp.br>
2024-12-04 14:15:42 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/firebase_options.dart';
-<<<<<<< HEAD
-import 'package:my_app/pages/cadastro.dart';
-=======
 import 'package:my_app/controllers/auth/mainPage.dart';
-import 'package:my_app/views/pages/cadastroCategoria.dart';
->>>>>>> b4eef3107cdb6e3ec843c349cd126563f4efbf92
+import 'package:my_app/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
